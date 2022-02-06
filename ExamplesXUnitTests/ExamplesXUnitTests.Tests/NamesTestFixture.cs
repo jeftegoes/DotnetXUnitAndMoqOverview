f0bf575959c7cf68399d00acec13f8be23926e7a
jeftegoes <jefte.goes.dev@outlook.com>
2022-02-05 23:38:39 -0300

    Study continuation.

---
+++ ExamplesXUnitTests/ExamplesXUnitTests.Tests/NamesTestFixture.cs
@@ -0,0 +1,7 @@
+namespace ExamplesXUnitTests.Tests
+{
+    public class NamesFixture
+    {
+        public readonly Names _names = new Names();
+    }
+}
--- ExamplesXUnitTests/ExamplesXUnitTests.Tests/NamesTests.cs
+++ ExamplesXUnitTests/ExamplesXUnitTests.Tests/NamesTests.cs
@@ -2,13 +2,19 @@ using Xunit;
 
 namespace ExamplesXUnitTests.Tests
 {
-    public class NamesTests
+    public class NamesTests : IClassFixture<NamesFixture>
     {
+        private readonly NamesFixture _namesFixture;
+
+        public NamesTests(NamesFixture namesFixture)
+        {
+            _namesFixture = namesFixture;
+        }
+
         [Fact]
         public void MakeFullName_GivenName_ReturnString()
         {
-            var names = new Names();
-            var result = names.MakeFullName("Jefte", "Goes");
+            var result = _namesFixture._names.MakeFullName("Jefte", "Goes");
 
             Assert.Equal("Jefte Goes", result);
             Assert.Equal("jefte goes", result, true);
@@ -21,20 +27,17 @@ namespace ExamplesXUnitTests.Tests
         [Fact]
         public void NickName_MustBeNull()
         {
-            var names = new Names();
+            Assert.Null(_namesFixture._names.NickName);
 
-            Assert.Null(names.NickName);
+            _namesFixture._names.NickName = "Baba Yaga";
 
-            names.NickName = "Baba Yaga";
-
-            Assert.NotNull(names.NickName);
+            Assert.NotNull(_namesFixture._names.NickName);
         }
 
         [Fact]
         public void MakeFullName_AlwaysReturnValue()
         {
-            var names = new Names();
-            var result = names.MakeFullName("Jefte", "Goes");
+            var result = _namesFixture._names.MakeFullName("Jefte", "Goes");
             Assert.NotNull(result);
             Assert.True(!string.IsNullOrEmpty(result));
             Assert.False(string.IsNullOrEmpty(result));
